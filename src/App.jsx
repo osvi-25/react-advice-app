f9ccfaf8408aea5c04f4eb1d6624ebacb6b7cb80
osvi-25 <sherminosvi25@gmail.com>
2025-09-10 22:07:35 +0530

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { AdviceApp } from './projects/adviceApp/AdviceApp'
+
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <AdviceApp />
+    </>
+  )
+}
+
+export default App
