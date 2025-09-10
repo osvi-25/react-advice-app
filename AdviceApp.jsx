ac14a1cb42190aa75c4ad9c3f248a255703f202e
osvi-25 <sherminosvi25@gmail.com>
2025-09-10 21:56:49 +0530

    Initial Commit : React Advice App

---
+++ AdviceApp.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './AdviceApp.css'
+
+export const AdviceApp = () => {
+    const [advice, setAdvice] = useState("Please click a button for advice");
+    const [count, setCount] = useState(0);
+
+    async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice)
+    setCount((c)=>c+1)
+   }
+
+   useEffect(function () {
+    getAdvice();
+   }, []);
+
+  return (
+        <div className="app-container">
+            <p className="advice">{advice}</p>
+            <button onClick={getAdvice}>Get Advice</button>
+            <p>You read <b>{count}</b> pieces of advice</p>
+        </div>
+  )
+}
